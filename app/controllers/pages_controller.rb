f00dfeee9c37d57bbe692feaf7bda9d70a036d4a
AkitaOnRails <fabioakita@gmail.com>
2011-02-24 17:25:18 -0300

    removing unnecessary filter from the controller that remained from the partials version of the site

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,4 @@
 class PagesController < ApplicationController
-  before_filter :load_sidebar
-
   def index
     @page = Page.by_slug("homepage")
     render :action => "show"
@@ -8,17 +6,5 @@ class PagesController < ApplicationController
 
   def show
     @page = Page.by_slug(params[:id])
-  end
-
-private
-
-  def load_sidebar
-    @advertising = Advertising.random
-    @poll = if params[:id]
-      Poll.last
-    else
-      Poll.first
-    end
-    @links = Link.order(:id).all
   end
 end
